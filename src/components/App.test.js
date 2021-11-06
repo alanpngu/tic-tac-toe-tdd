9cfd55cb6e224cf9aa08b81eb8e5b10a1a88baa1
Alan Nguyen <alanpngu@usc.edu>
2021-11-05 21:15:07 -0700

    Test case 'Find tic-tac-toe text': Fail

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,4 +6,10 @@ describe('render the initial app', () => {
   it('renders App', () => {
     shallow(<App />);
   });
+
+  it('finds tic-tac-toe-text', () => {
+    const wrapper = shallow(<App/>);
+    const header = (<h1>Tic-Tac-Toe</h1>);
+    expect(wrapper.contains(header)).toEqual(true);
+  });
 });
