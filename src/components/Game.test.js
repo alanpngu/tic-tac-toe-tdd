6cb0a4a1380fe49fb955330aac0a2d8578eecfec
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 13:17:24 -0700

    Test case 'Detect a Board in Game': Fail

--- src/components/Game.test.js
+++ src/components/Game.test.js
@@ -1,8 +1,13 @@
 import Game from './Game';
 import { shallow } from 'enzyme';
+import Board from './Board'
 
 describe('render Game component', () => {
   it('renders Game', () => {
     shallow(<Game />);
+  })
+  it ('game contains a Board', () => {  
+    const component = shallow(<Game/>)
+    expect(component.find(Board)).toHaveLength(1);
   })
 });
