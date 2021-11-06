a4e3295982ddd62527872ddcdb6242b13ef1ce3a
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 03:16:27 -0700

    Test case 'Detect if three rows exist on Board': Fail

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,6 +1,6 @@
 import Board from './Board';
 import Square from './Square';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe('render Board component', () => {
   it('Board component rendering', () => {
@@ -9,5 +9,9 @@ describe('render Board component', () => {
   it('detect a Square in Board', () => {
     const component = shallow(<Board />);
     expect(component.contains(<Square />)).toEqual(true);
+  });
+  it('render three rows', () => {
+    const component = mount(<Board />);
+    expect(component.find('div.boardRow').length).toBe(3);
   });
 });
