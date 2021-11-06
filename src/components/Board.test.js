29d54633423d7dc60e000bca1eb45adb9878555d
Alan Nguyen <alanpngu@usc.edu>
2021-11-05 21:22:40 -0700

    Test case 'Detect if Board component working': Pass

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -3,7 +3,7 @@ import Board from './Board';
 import { shallow } from 'enzyme';
 
 describe('render Board component', () => {
-  it('renders Board', () => {
+  it('Board component rendering', () => {
     shallow(<Board />);
   })
 });
