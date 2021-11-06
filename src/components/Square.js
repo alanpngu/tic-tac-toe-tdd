5522c0bbc9092464c87afe4a2f79ae2cdabc8ca8
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 01:39:44 -0700

    Test case 'If X/O passed, check if rendered': Pass

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Square(props) {
     return (
         <button onClick={() => props.onClick()}>
+            {props.value}
         </button>
     )
 }
