e3f492216d1167fd5828438a6aaccc57f1c6c462
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 00:41:41 -0700

    Test case 'Detect if Board contains a square': Fail

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,8 +1,13 @@
 import Board from './Board';
+import Square from './Square';
 import { shallow } from 'enzyme';
 
 describe('render Board component', () => {
   it('Board component rendering', () => {
     shallow(<Board />);
-  })
+  });
+  it('detect a Square in Board', () => {
+    const component = shallow(<Board />);
+    expect(component.contains(<Square />)).toEqual(true);
+  });
 });
