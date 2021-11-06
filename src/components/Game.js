fb83cfabd5beb44e81ca9e685ec7e520ba0ef7f8
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 13:18:44 -0700

    Test case 'Detect a Board in Game': Pass

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
-
+import Board from './Board'
 
 export class Game extends Component {
     render() {
         return (
             <div name = "game">
+                <Board />
             </div>       
         );
     }
