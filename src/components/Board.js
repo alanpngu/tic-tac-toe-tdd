d33d0ad1e59b76fa988dffb127b9722fc988abcd
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 00:42:39 -0700

    Test case 'Detect if Board contains a square': Pass

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react'
+import Square from './Square'
 import React, {Component} from 'react'
 
 export class Board extends Component {
     render() {
         return (
             <div name = "board">
+                <div>
+                    <Square />
+                </div>
             </div>       
         );
     }
