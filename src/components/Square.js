2618c9fc8c77b8511654182b4f876e2f03e4bfdc
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 15:06:07 -0700

    Add styling for Squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Square.css'
 export default function Square(props) {
     return (
-        <button onClick={() => props.onClick()}>
+        <button className = 'square' onClick={() => props.onClick()}>
             {props.value}
         </button>
     )
