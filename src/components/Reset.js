37c3a1cec53f0666841113e69533e0aba6196d31
Alan Nguyen <alanpngu@usc.edu>
2021-11-07 01:02:12 -0800

    Test case 'Reset button has label': Pass

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Reset.css'
 
 export default function Reset(props) {
     return (
         <button className = 'resetButton' onClick={() => props.onClick()}>
             {props.value}
+            Reset
         </button>
     )
 }
