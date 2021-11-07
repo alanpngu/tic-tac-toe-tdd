1384154c09253baebe4a1006bc5ebb7da4084a41
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 23:54:47 -0700

    Test case: 'Basic reset button functionalities': Fail

---
+++ src/components/Reset.test.js
@@ -0,0 +1,22 @@
+import Reset from './Reset';
+import { shallow } from 'enzyme';
+
+const mockCallback = jest.fn();
+
+describe('render Reset component', () => {
+  it('renders Reset', () => {
+    shallow(<Reset />);
+  })
+
+  it('detect if button exists', () => {
+    const wrapper = shallow(<Reset />)
+    expect(wrapper.find('resetButton').length).toEqual(1);
+  })
+
+  it('detect if button is clickable', () => {
+    const wrapper = shallow(<Reset onClick = {mockCallback}/>)
+    const buttonComponent = wrapper.find('button')
+    buttonComponent.simulate('click')
+    expect(mockCallback).toHaveBeenCalled();
+  })
+})
