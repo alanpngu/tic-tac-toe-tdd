5903eed8c341377c378a43ad9aa591e5c50a5e2b
Alan Nguyen <alanpngu@usc.edu>
2021-11-06 17:39:46 -0700

    Test case 'Initial state of squares is null': Pass

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,10 +2,17 @@ import React, {Component} from 'react'
 import Board from './Board'
 
 export class Game extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
     render() {
         return (
             <div name = "game">
-                <Board />
+                <Board squares = {this.state.squares}/>
             </div>       
         );
     }
